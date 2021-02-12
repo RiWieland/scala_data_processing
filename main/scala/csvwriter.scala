53c1073b30d04c408025c99a1da9311d26df7e5b
rwie1and <61945371+rwie1and@users.noreply.github.com>
2021-02-12 23:01:45 +0100

    init main function

--- main/scala/csvwriter.scala
+++ main/scala/csvwriter.scala
@@ -3,5 +3,13 @@ package csvwriter
 
 
 object csv_writer{
+  def init_for_csv(): ListBuffer[Array[String]] = {
+      val listOfRecords = new ListBuffer[Array[String]]()
+      val CSVschema = Array("company", "analyst", "valuation_new", "rating", "valuation_percent", "price_hist", "analyst_name", "date_topic", "date_release", "date_publish")
+      listOfRecords += CSVschema
+
+    return listOfRecords
+}
+
 
 }
