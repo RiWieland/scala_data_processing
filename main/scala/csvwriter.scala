b2077d8acc01cf2ee45fe1c64b9a678d13395505
rwie1and <61945371+rwie1and@users.noreply.github.com>
2021-02-17 22:22:06 +0100

    update csv writer

--- main/scala/csvwriter.scala
+++ main/scala/csvwriter.scala
@@ -7,8 +7,10 @@ import au.com.bytecode.opencsv.CSVWriter
 import java.io.{BufferedWriter, FileWriter}
 import scala.collection.JavaConverters._
 
+import index.index
 
 object csv_writer{
+
   def init_for_csv(): ListBuffer[Array[String]] = {
       val listOfRecords = new ListBuffer[Array[String]]()
       val CSVschema = Array("company", "analyst", "valuation_new", "rating", "valuation_percent", "price_hist", "analyst_name", "date_topic", "date_release", "date_publish")
@@ -16,8 +18,16 @@ object csv_writer{
 
     return listOfRecords
 }
+// : ListBuffer[Array[String]]
   def figures_to_csv(listOfRecords: ListBuffer[Array[String]], stock: index.Index_object) = {
+      println("CSV writer called for ", stock.Stock)
+      val date = java.time.LocalDate.now
+      val outputFile = new BufferedWriter(new FileWriter(s"/Users/Rich/Desktop/PlanD/Analyst_feed/analyst_${stock.Stock}_${date}.csv"))
+      val csvWriter = new CSVWriter(outputFile)
+      
+      val listOfRecords_fin = listOfRecords.reverse :+ Array("company", "analyst", "valuation_new", "rating", "valuation_percent", "price_hist", "analyst_name") 
 
-}
-
+      csvWriter.writeAll(listOfRecords_fin.reverse.toList.asJava)
+      outputFile.close
+  } 
 }
