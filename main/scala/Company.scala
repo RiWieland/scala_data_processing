d7118164695c40c78964452faa609cbac4c047a4
rwie1and <61945371+rwie1and@users.noreply.github.com>
2021-02-22 21:39:08 +0100

    add function max page

--- main/scala/Company.scala
+++ main/scala/Company.scala
@@ -2,6 +2,23 @@
 
 object stock_analyst_links {
 
+  case class link(Link: String)
+  
+
+  def get_max_page(link_raw: Elements ) : Option[Int] = {
+
+    val list_num = new ListBuffer[Option[Int]]()
+
+      for (entry: Element <- link_raw.asScala){
+
+        if (entry.attr("href").toLowerCase.contains("p=")){
+          val temp = entry.attr("href").substring(3)
+          list_num += tryToInt(temp)
+        }
+    }
+    return list_num.max
+  }
+
 
   }
 
