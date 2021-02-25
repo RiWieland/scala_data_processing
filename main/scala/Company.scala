b5ed564e9142ee969b3c21f74299973357431ddb
rwie1and <61945371+rwie1and@users.noreply.github.com>
2021-02-26 00:01:14 +0100

    function get history

--- main/scala/Company.scala
+++ main/scala/Company.scala
@@ -3,6 +3,11 @@
 object stock_analyst_links {
 
   case class link(Link: String)
+
+  def tryToInt( s: String ) = Try(s.toInt).toOption
+
+  def create_landing_link(company: String, page: Int) : String = s"XXX"
+
   
 
   def get_max_page(link_raw: Elements ) : Option[Int] = {
@@ -19,6 +24,37 @@ object stock_analyst_links {
     return list_num.max
   }
 
+  def get_webpage_content(link:String, html_element:String) : Elements = {
+    val doc: Document = Jsoup.connect(link).userAgent("Mozilla").get()
+    val link_raw: Elements = doc.select(html_element)
+
+    return link_raw
+  }
+
+  def get_history(company: String) : ListBuffer[link] ={
+  
+    // Init: 
+    val links_list = new ListBuffer[link]()
+    val max_range = get_max_page(get_webpage_content(create_landing_link(company, 1), "a[href]"))
+
+    for( page <- 1 to max_range.getOrElse(1) ){ //max_range.getOrElse(1)){//max_range.getOrElse(1)){
+      
+      val link_raw : Elements = get_webpage_content(create_landing_link(company, page), "a[href]")
+
+      println("crawling page " + page + " for company " + company)
+
+      for (entry: Element <- link_raw.asScala){      
+        
+        if (entry.attr("href").toLowerCase.contains("/analyse/")){
+          
+          links_list += link("https://www.finanzen.net" + entry.attr("href")) 
+
+        }
+      }
+    }
+    return links_list
+  }
+}
 
   }
 
