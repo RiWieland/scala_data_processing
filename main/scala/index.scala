ba44b33a9c00ff94c2164dc0b6c17ee81be72e6c
rwie1and <61945371+rwie1and@users.noreply.github.com>
2021-03-05 14:24:48 +0100

    add function page range

--- main/scala/index.scala
+++ main/scala/index.scala
@@ -10,5 +10,19 @@ object index{
 
   def tryToInt(s: String) = Try(s.toInt).toOption
 
+  def get_pages_range(link_raw: Elements ) : Option[Int] = {
+
+      val list_num = new ListBuffer[Option[Int]]()
+
+        for (entry: Element <- link_raw.asScala){
+
+          if (entry.attr("href").toLowerCase.contains("p=")){
+            val temp = entry.attr("href").substring(3)
+            list_num += tryToInt(temp)
+          }
+      }
+      val max = if (list_num.nonEmpty) list_num.max else Some(1)
+      return max
+    }
 
 }
