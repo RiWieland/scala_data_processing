ccb871c829eb4625786a391bf6251481cdac1faa
rwie1and <61945371+rwie1and@users.noreply.github.com>
2022-03-15 19:01:51 +0100

    added index list

--- main/scala/Main.scala
+++ main/scala/Main.scala
@@ -2,6 +2,7 @@
 
 
 object crawl_analyst_statement {
+  val index_list: List[String] = List("tecdax", "s&p_500")//, "sdax",  "dow_jones", "mdax", "nasdaq_100", "euro_stoxx_50", "smi", "atx", "cac_40", "nikkei_225")         
 
   def main(args: Array[String]){
 
