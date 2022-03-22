2ccb27e7c1c1c5114caf87497c16f392c6e712e0
rwie1and <61945371+rwie1and@users.noreply.github.com>
2022-03-22 19:21:39 +0100

    adding function for extrating figures

--- main/scala/analyst.scala
+++ main/scala/analyst.scala
@@ -22,3 +22,31 @@ import org.jsoup.select.Elements;
       val dat  = extract_date(link)
     
     figures(fig(0).toString,fig(1).toString,fig(2).toString.toDouble,fig(3).toString,fig(4).toString.toDouble,fig(5).toString.toDouble, fig(6).toString, dat(0).toString, dat(1), dat(2) )
+
+      def extract_figures(text_list: ListBuffer[String]): Array[Any] = {
+
+    val company = try {transform(text_list(0)).get.split(" : ").mkString(", ").split(", ").toList(0).toString
+                      } catch {case e: Exception => "None"}
+
+    val analyst = try {transform(text_list(0)).get.split(" : ").mkString(", ").split(", ").toList(1).toString
+                      } catch {case e: Exception => "None"}
+
+    val valuation_new = try {transform(text_list(0)).get.split(" : ").mkString(", ").split(", ").toList(2).toDouble
+                        } catch {case e: Exception => 0.0}
+
+    val rating = try {transform(text_list(0)).get.split(" : ").mkString(", ").split(", ").toList(3).toString
+                        } catch {case e: Exception => "None"}
+
+
+    val valuation_percent = try {transform(text_list(0)).get.split(" : ").mkString(", ").split(", ").toList(4).toDouble
+                        } catch {case e: Exception => 0.0}
+
+    val price_hist = try {transform(text_list(0)).get.split(" : ").mkString(", ").split(", ").toList(5).toDouble
+                        } catch {case e: Exception => 0.0}
+    
+    val analyst_name = try {transform(text_list(0)).get.split(" : ").mkString(", ").split(", ").toList(6).toString
+                        } catch {case e: Exception => "None"}
+
+    return Array(company, analyst, valuation_new, rating, valuation_percent, price_hist, analyst_name)
+
+  }
