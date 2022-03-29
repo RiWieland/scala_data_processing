1a80033780a347dff8442c9bedf02d8d2f1529ed
rwie1and <61945371+rwie1and@users.noreply.github.com>
2022-03-29 10:23:22 +0200

    update index.scala

--- main/scala/index.scala
+++ main/scala/index.scala
@@ -10,14 +10,14 @@ import scala.util.Try
 
 
 object index{
-
+  
   case class Index_object(Index: String, Stock: String)
 
-  def landing_index(index: String, page: Int ) : String = s"XXX/${index}/werte?p=${page}"
+    def landing_index(index: String, page: Int ) : String = s"https://www.finanzen.net/index/${index}/werte?p=${page}"
+    
+    def tryToInt(s: String) = Try(s.toInt).toOption
 
-  def tryToInt(s: String) = Try(s.toInt).toOption
-
-  def get_pages_range(link_raw: Elements ) : Option[Int] = {
+    def get_pages_range(link_raw: Elements ) : Option[Int] = {
 
       val list_num = new ListBuffer[Option[Int]]()
 
@@ -54,8 +54,8 @@ object index{
             && entry.attr("href").toLowerCase.contains("/aktien/") 
             && entry.attr("href").toLowerCase.length < 100){
                             
-              Stocks_landing += Index_object(market_index.toString, "XXX" + entry.attr("href"))
-              Stocks_analysts += Index_object(market_index.toString, "XXX" + entry.attr("href").replace("-aktie", "-analysen").replace("/aktien", ""))
+              Stocks_landing += Index_object(market_index.toString, "https://www.finanzen.net" + entry.attr("href"))
+              Stocks_analysts += Index_object(market_index.toString, "https://www.finanzen.net/analysen" + entry.attr("href").replace("-aktie", "-analysen").replace("/aktien", ""))
 
               company_list += Index_object(market_index.toString, entry.attr("href").replace("-aktie", "").replace("/aktien", "").replace("/",""))
               
@@ -66,45 +66,4 @@ object index{
         return company_list
 
       }
-
-  def extract_date(link: String) : Array[String] = {
-
-      //val link = "https://www.finanzen.net/analyse/carl_zeiss_meditec_halten-dz_bank_747698"
-      val doc: Document = Jsoup.connect(link).userAgent("Mozilla").ignoreHttpErrors(true).get()
-      val link_List: Elements = doc.select("div")
-      
-      val text_list = new ListBuffer[String]()
-      
-      for (entry: Element <- link_List.asScala){
-        
-        if (entry.text.contains("Original-Studie")){ // || entry.text.contains("Der Analyst Cascend Securities") ){
-          text_list += entry.text
-        }
-      }
-
-      val eles : Elements = doc.select("div.optionBar") //pull-left mright-20
-      
-      val topic_list = new ListBuffer[String]()
-
-      for (entry: Element <- eles.asScala){
-        topic_list += entry.text
-
-      }
-
-      val relevant_topic = try{topic_list(0)} catch {case e: Exception => "None"}
-
-      val date_topic = try{relevant_topic.slice(relevant_topic.indexOfSlice("mright-20")+1, relevant_topic.indexOfSlice("mright-20")+17)}
-                              catch {case e: Exception => "None"}
-
-      
-      val relevant_text = try {text_list(1)} catch {case e: Exception => "None"}
-
-      val date_release = try {relevant_text.slice(relevant_text.indexOfSlice("Veröffentlichung der")+38, relevant_text.indexOfSlice("Veröffentlichung der")+63)
-                                } catch {case e: Exception => "None"}
-      val date_publish = try {relevant_text.slice(relevant_text.indexOfSlice("Erstmalige Weitergabe")+43, relevant_text.indexOfSlice("Erstmalige Weitergabe")+67) 
-                                } catch {case e: Exception => "None"}
-      
-      return Array(date_topic, date_release, date_publish)
-      }
-
 }
